1fdd42410730042fa11a28ebb8db851ad53a7535
Adrián Antón <adrtler@gmail.com>
2022-02-19 11:37:20 +0100

    Agregando reglas de validación usando anotaciones

--- src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
+
+import javax.validation.Valid;
 
 import com.bolsaideas.springboot.app.models.dao.IClienteDao;
 import com.bolsaideas.springboot.app.models.entity.Cliente;
@@ -34,9 +38,26 @@ public class ClienteController {
 		return "form";
 	}
 
+	/**
+	 * El @BindingResult siempre tiene que ir junto al objeto que está validado
+	 * con @Valid
+	 * 
+	 * El @ModelAttribute solo es necesario si el nombre con el que lo pasamos a la
+	 * vista en el método anterior es distinto al que le
+	 * estamos dando (Nombre de la clase) en el método receptor del form
+	 * 
+	 * @param cliente
+	 * @param result
+	 * @return
+	 */
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
-		clienteDao.save(cliente);
+	public String guardar(@Valid @ModelAttribute(value = "cliente") Cliente clienteN, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";
+		}
+		clienteDao.save(clienteN);
 		return "redirect:listar";
 	}
 }
--- src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -25,16 +28,21 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
 	private String nombre;
 
+	@NotEmpty
 	private String apellido;
 
+	@NotEmpty
+	@Email
 	private String email;
 
 	/**
 	 * Si no especificamos el column name el nombre en la tabla será el mismo que la
 	 * variable. El campo temporal indica como vamos a guardar la fecha
 	 */
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
